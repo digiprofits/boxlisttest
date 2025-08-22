b07439c1eb38c941d9de283bdfd0797e5251aded
digiprofits <admin@digiprofitsagency.com>
2025-08-22 18:02:46 +1000

    Create inApp.ts

---
+++ src/inApp.ts
@@ -0,0 +1,5 @@
+export const isInAppBrowser = /FBAN|FBAV|FB_IAB|Instagram|Line|MicroMessenger|Snapchat|Twitter|WhatsApp|Messenger|Weibo|WeChat|TikTok|Telegram/i.test(
+  navigator.userAgent
+);
+export const isIOS = /iphone|ipad|ipod/i.test(navigator.userAgent);
+export const isAndroid = /android/i.test(navigator.userAgent);
