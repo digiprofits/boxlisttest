9fec753b8199a7593f1742f7f8d75adc89443906
digiprofits <admin@digiprofitsagency.com>
2025-08-23 11:06:54 +1000

    Update inApp.ts

--- src/utils/inApp.ts
+++ src/utils/inApp.ts
@@ -1,6 +1,3 @@
-// Simple in-app browser & platform detection
+// src/utils/inApp.ts
 export const isInAppBrowser = /FBAN|FBAV|FB_IAB|Instagram|Line|MicroMessenger|Snapchat|Twitter|WhatsApp|Messenger|Weibo|WeChat|TikTok|Telegram/i
   .test(navigator.userAgent);
-
-export const isIOS = /iphone|ipad|ipod/i.test(navigator.userAgent);
-export const isAndroid = /android/i.test(navigator.userAgent);
