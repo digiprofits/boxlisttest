0bdb7440faee3da057f5d8ff8fa3b24b3cf3fcac
digiprofits <admin@digiprofitsagency.com>
2025-08-26 16:38:27 +1000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,32 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './styles/index.css'
-import App from './App'
-import Moves from './routes/Moves'
-import Boxes from './routes/Boxes'
-import BoxDetail from './routes/BoxDetail'
-import Items from './routes/Items'       // NEW
-import Search from './routes/Search'     // NEW
-import Settings from './routes/Settings'
-import Labels from './routes/Labels'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './styles/index.css';
+import App from './App';
+import Moves from './routes/Moves';
+import Boxes from './routes/Boxes';
+import BoxDetail from './routes/BoxDetail';
+import Rooms from './routes/Rooms';
+import RoomBoxes from './routes/RoomBoxes';
+import Search from './routes/Search';
+import Settings from './routes/Settings';
+import Labels from './routes/Labels';
 
 const router = createBrowserRouter([
-  { path: '/', element: <App />,
+  {
+    path: '/',
+    element: <App />,
     children: [
       { index: true, element: <Moves /> },
+      { path: 'moves/:moveId/rooms', element: <Rooms /> },
+      { path: 'moves/:moveId/rooms/:roomId/boxes', element: <RoomBoxes /> },
       { path: 'moves/:moveId/boxes', element: <Boxes /> },
       { path: 'moves/:moveId/boxes/:boxId', element: <BoxDetail /> },
-      { path: 'moves/:moveId/items', element: <Items /> },     // NEW
-      { path: 'moves/:moveId/search', element: <Search /> },   // NEW
+      { path: 'moves/:moveId/search', element: <Search /> },
       { path: 'moves/:moveId/settings', element: <Settings /> },
       { path: 'moves/:moveId/labels', element: <Labels /> },
-    ]
-  }
-])
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
